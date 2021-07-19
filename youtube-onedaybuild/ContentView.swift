741a6ce9559d35d66f7ffa8e34524576c627c99e
quang <quang>
2021-07-19 13:44:46 +0700

    Initial Commit

---
+++ youtube-onedaybuild/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  youtube-onedaybuild
+//
+//  Created by Le Minh Quang  on 19/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ youtube-onedaybuild/youtube_onedaybuildApp.swift
@@ -0,0 +1,17 @@
+//
+//  youtube_onedaybuildApp.swift
+//  youtube-onedaybuild
+//
+//  Created by Le Minh Quang  on 19/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct youtube_onedaybuildApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
